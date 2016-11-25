036829b94e4154c6139411f6a016191eee6113b4
Mayomi Tian <Mayomi Tian>
2016-11-26 01:41:36 +0900

    Footer 修正

--- inc/footer.php
+++ inc/footer.php
@@ -2,8 +2,8 @@
 include('config.php'); 
 ?>
 <div align="center">
-&copy; <strong><?php echo $mc_name; ?></strong> 2016. All rights reserved.<br>
-<a href="http://www.miitbeian.gov.cn/" target="_blank"><?php echo $mc_beian; ?></a>
+&copy; <strong><?php echo $global_name; ?></strong> 2016. All rights reserved.<br>
+<a href="http://www.miitbeian.gov.cn/" target="_blank"><?php echo $global_beian; ?></a>
 </div>
 <script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
